948fb83694e349f6f2b9f736b8a19a9c40cd23a6
rizaru22 <rizaru.2.2@gmail.com>
2026-02-02 11:12:10 +0700

    UPDATE Filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' =>  env('PUBLIC_DISK_ROOT', public_path('storage')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
