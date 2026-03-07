4d3b2529e70e7a7f970515ff4cad45e53493ff35
rizaru22 <rizaru.2.2@gmail.com>
2026-03-07 16:46:16 +0700

    Change class name PpdbController

--- app/Http/Controllers/PpdbController.php
+++ app/Http/Controllers/PpdbController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
-class ppdbController extends Controller
+class PpdbController extends Controller
 {
     //
     public function index()
