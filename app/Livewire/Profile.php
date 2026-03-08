b46ab2cb60f7ace7160bf3d7203d42a3d2361463
rizaru22 <rizaru.2.2@gmail.com>
2026-03-08 18:00:18 +0700

    FIX Display Berita Terbaru di Profil Sekolah

--- app/Livewire/Profile.php
+++ app/Livewire/Profile.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 class Profile extends Component{
     
     public function render(){
-        $berita_terbaru=\App\Models\News::latest()->take(3)->get();
+        $berita_terbaru=\App\Models\News::orderBy('published_at', 'desc')->take(3)->get();
         $prestasi_terbaru=\App\Models\Achievement::orderBy('date', 'desc')->take(3)->get();
         $kompetensi_keahlians=\App\Models\KompetensiKeahlian::all();
         return view('livewire.profile',compact('kompetensi_keahlians', 'berita_terbaru','prestasi_terbaru'))
